cabe9dbc6d7c3cdd15a69cf84005d607ba12fc32
Luis Rodas <emmanuelcamposeco@gmail.com>
2019-04-29 20:03:52 -0600

    Seccion 4

---
+++ sockets/socket.ts
@@ -0,0 +1,20 @@
+import { Socket } from 'socket.io';
+import socketIO from 'socket.io';
+
+
+export const desconectar = (cliente: Socket) => {
+    cliente.on('disconnect', () => {
+        console.log('cliente desconectado');
+    })
+}
+
+// Escuchar mensaje
+export const mensaje = (cliente: Socket, io: socketIO.Server) => {
+    cliente.on('mensaje', (payload)=> {
+
+        console.log('Mensaje recibido', payload);
+
+        io.emit('mensaje-nuevo', payload);
+    });
+
+};
